93bf02ca1f6b0d1986ad478e1f400100fe306e1f
JCNT Creations <jcnt.austria@gmail.com>
2025-07-30 22:31:49 +0200

    Main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "web/webserver.hpp"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
@@ -5,6 +6,7 @@
 #include <cppconn/statement.h>
 #include <iostream>
 #include <mysql_connection.h>
+#include <thread>
 
 int main() {
 	sql::Driver *driver = get_driver_instance();
@@ -12,28 +14,31 @@ int main() {
 	sql::Statement *stmt;
 	sql::ResultSet *res;
 	try {
-		std::unique_ptr<sql::Connection> con(driver->connect("192.168.10.50:3306", "test", "test")); // FILL IN HERE
+		std::unique_ptr<sql::Connection> con(driver->connect("192.168.56.1:3306", "test", "test"));
 		if (con != NULL) {
 			std::cout << "MySQL Connected successfully!\n";
 		}
 
-		stmt = con->createStatement();
+		/*stmt = con->createStatement();
 		res = stmt->executeQuery("SELECT * FROM test.test");
 		// res = stmt->executeQuery("SELECT * FROM test");
 		while (res->next()) {
 			/*std::cout << "\t... MySQL replies: ";
 			std::cout << res->getString("_message") << std::endl;
 			std::cout << "\t... MySQL says it again: ";
-			std::cout << res->getString(1) << std::endl; */
+			std::cout << res->getString(1) << std::endl; * /
 			std::cout << "first: " << res->getInt("first") << "\nsecond: " << res->getInt("second") << std::endl;
-		}
+		}*/
 		/*delete res;
 		delete stmt;
 		delete con;*/
 
 		// Prepared statement
-		/*webserver *ws = new webserver(con.get(), 8080);
-		ws->Test();*/
+		WebServer webserver(con.get(), 8080);
+		std::thread webThread = std::thread(webserver);
+		// ws->Test();
+		std::cout << "Main: Webserver started on port 8080" << std::endl;
+		webThread.join();
 
 	} catch (sql::SQLException &e) {
 		std::cerr << "MySQL was NOT started or Incorrect credentials.\n" << e.what() << std::endl;
